7be9b903d8727af7183d9c2e7aef047c340f226d
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-13 22:35:08 +0530

    Create continue.cpp

---
+++ continue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  
+#include<stdio.h>
+using namespace std;  
+int main()  
+{  
+  clrscr();
+     for(int i=1;i<=10;i++){      
+            if(i==5)
+            {      
+                continue;      
+            }      
+            cout<<i<<"\n";      
+        }        
+}
